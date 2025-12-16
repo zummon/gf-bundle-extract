aa90284a7359f5ac2ae015a2194d8642efe74bce
zummon <zummon.space@gmail.com>
2025-12-16 23:05:56 +0700

    save

--- src/App.svelte
+++ src/App.svelte
@@ -1,63 +1,186 @@
 <script>
 	const { read, utils } = globalThis.XLSX;
+	const routes = ["เงินในงบฯ"];
 
+	function formatDate(value, option) {
+		return new Date(value).toLocaleDateString("th", {
+			day: "numeric",
+			month: "short",
+			year: "numeric",
+			...option,
+		});
+	}
 	function upload(e, callback) {
 		const file = e.currentTarget.files[0];
 		file.arrayBuffer().then((rawTrans) => {
 			const fileTrans = read(rawTrans, { cellDates: true });
 			const sheetName = fileTrans.SheetNames[0];
 			const worksheet = fileTrans.Sheets[sheetName];
-			callback(utils.sheet_to_json(worksheet, { header: 1 }))
+			let aoa = utils.sheet_to_json(worksheet, { header: 1 });
+			callback(aoa);
 		});
 	}
+	function clear() {
+		journal = [];
+	}
 
-	let dataGL = $state([[]]);
+	let journal = $state([]);
+	let route = $state(routes[0]);
 
 	let report = $derived.by(() => {
-		let desc = {}
-		let pay = []
-		let receive = []
-		let rowindex = 0
-		for (const cells of dataGL.slice(1)) {
-			if (['K0','KA','KC','KL'].includes(cells[5])) {
-				desc[cells[3]] = cells[15]
-				receive.push({ date: cells[6], group: cells[12], desc: cells[15] })
+		let detail = {};
+		let allowed = [];
+		let rowindex = 0;
+		for (const cells of journal.slice(1)) {
+			const [, accountName, desc] = cells[1].split("\n");
+			const [docNo] = cells[2].split("\n");
+			let [docDate, accountCode, sourceFund] = cells[4].split("\n");
+			const [docType] = cells[5].split("\n");
+			const [refNo] = cells[6].split("\n");
+			const [, account] = cells[7].split("\n");
+			const [, debit] = cells[11].split("\n");
+			const [, credit] = cells[12].split("\n");
+			const [day, indexmonth, year] = docDate.split(".");
+			docDate = new Date(year - 543, indexmonth, day);
+			if (!isNaN(accountCode)) {
+				if (["K0", "KA", "KC", "KL"].includes(docType)) {
+					detail[docNo] = desc;
+					allowed.push({
+						docDate,
+						docNo,
+						refNo,
+						sourceFund,
+						desc,
+						debit,
+						credit,
+					});
+				} else if (docType == "PM") {
+					allowed.push({
+						docDate,
+						docNo,
+						refNo,
+						sourceFund,
+						desc: desc || detail[docNo],
+						debit,
+						credit,
+					});
+				}
 			}
-			if (cells[5] == 'PM') {
-				pay.push({ date: cells[6], group: cells[12], desc: desc[cells[11].slice(4)] })
-			}
-			rowindex += 1
+
+			rowindex += 1;
 		}
-		return { desc, pay, receive }
+		return { allowed };
 	});
 </script>
 
-<div class="">
-	<label for="">
-		NFI_DISPLAY_L การแสดงบรรทัดรายการบัญชีแยกประเภททั่วไป
-		<input
-			type="file"
-			class=""
-			accept="xlsx"
-			onchange={(e) => {
-				upload(e, (result) => {
-					dataGL = result.slice(6)
-				});
-			}}
-		/>
-	</label>
+<div class="p-4 flex flex-wrap gap-4 print:hidden">
+	<div class="">
+		<label>
+			NGL_RPT001 รายงานสมุดรายวันทั่วไป
+			<input
+				type="file"
+				class="cursor-pointer"
+				accept="xlsx"
+				onchange={(e) => {
+					upload(e, (aoa) => {
+						journal = aoa;
+					});
+				}}
+			/>
+		</label>
+	</div>
+	<div class="">
+		<button
+			class="cursor-pointer {journal[0]
+				? 'bg-orange-500'
+				: 'bg-zinc-500'} font-semibold text-white rounded px-1"
+			onclick={() => {
+				clear();
+			}}>Clear</button
+		>
+	</div>
+	{#each routes as value}
+		<div class="">
+			<button
+				class="cursor-pointer {route == value
+					? 'bg-zinc-500'
+					: 'bg-cyan-500'} font-semibold text-white rounded px-1"
+				onclick={() => {
+					route = value;
+				}}>{value}</button
+			>
+		</div>
+	{/each}
+	<div class="">
+		<button
+			class="cursor-pointer bg-cyan-500 font-semibold text-white rounded px-1"
+			onclick={() => {
+				print();
+			}}>Print</button
+		>
+	</div>
 </div>
 
-<div class="">
-	<table>
-		<tbody>
-			{#each report.pay as obj}
+<div class="px-4 flex flex-wrap gap-4 print:hidden">
+	<div class="">...</div>
+</div>
+
+<div class="p-4 print:p-0">
+	{#if route == routes[0]}
+		<table class="overflow-auto w-full">
+			<thead class="text-center">
 				<tr>
-					<td>{obj.date}</td>
-					<td>{obj.group}</td>
-					<td>{obj.desc}</td>
+					<td class="border">วันที่</td>
+					<td class="border">เลขที่ฎีกา</td>
+					<td class="border">ประเภทจ่าย</td>
+					<td class="border">งบ</td>
+					<td class="border">รายการ</td>
+					<td class="border">เดบิต</td>
+					<td class="border">เครดิต</td>
+					<td class="border">ยอดคงเหลือ</td>
+					<td class="print:hidden">เลขที่เอกสาร GF</td>
 				</tr>
-			{/each}
-		</tbody>
-	</table>
+			</thead>
+			<tbody>
+				{#each report.allowed as obj}
+					<tr class="">
+						<td class="border-x text-nowrap" style="border-bottom: 1px dotted;"
+							>{formatDate(obj.docDate, { year: undefined })}</td
+						>
+						<td class="border-x" style="border-bottom: 1px dotted;"
+							>{obj.refNo}</td
+						>
+						<td class="border-x" style="border-bottom: 1px dotted;"></td>
+						<td class="border-x" style="border-bottom: 1px dotted;"
+							>{obj.sourceFund}</td
+						>
+						<td class="border-x" style="border-bottom: 1px dotted;"
+							>{obj.desc}</td
+						>
+						<td class="border-x text-right" style="border-bottom: 1px dotted;"
+							>{obj.debit}</td
+						>
+						<td class="border-x text-right" style="border-bottom: 1px dotted;"
+							>{obj.credit}</td
+						>
+						<td class="border-x" style="border-bottom: 1px dotted;"></td>
+						<td class="print:hidden">{obj.docNo}</td>
+					</tr>
+				{/each}
+			</tbody>
+			<tfoot>
+				<tr class="">
+					<td class="border"></td>
+					<td class="border"></td>
+					<td class="border"></td>
+					<td class="border"></td>
+					<td class="border"></td>
+					<td class="border"></td>
+					<td class="border"></td>
+					<td class="border"></td>
+					<td class="print:hidden"></td>
+				</tr>
+			</tfoot>
+		</table>
+	{/if}
 </div>
